455cd9e37aaa0ff09e8a33b5acddfc8202a3f0d2
kagaya <kagaya85@foxmail.com>
2018-02-01 14:54:29 +0800

    补充注释

--- demo/data-ajax.js
+++ demo/data-ajax.js
@@ -1,23 +1,15 @@
+///////////////////////////////////////
+//与服务器请求数据时的ajax部分
+//回传数据为json格式的字符串 jsonData中为转换后的json数组
+//如需在其他位置使用时请设为全局变量
+//上传数据部分暂未完成测试
+//kagaya 2018年2月1日
+//////////////////////////////////////
 const uploadURL = "upload.php";
 const downloadURL = "download.php";
 
-// function getXMLHttpRequest() {
-//     var xmlhttp;
-//     if (window.XMLHttpRequest) {
-//         // IE7+, Firefox, Chrome, Opera, Safari 浏览器执行代码
-//         xmlhttp = new XMLHttpRequest();
-//     }
-//     else {
-//         // IE6, IE5 浏览器执行代码
-//         xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-//     }
-
-//     return xmlhttp;
-// }
-
 function showData(Lng, Lat, Ref = false) {
-    // var xmlhttp;
-    var jsonData;
+    var jsonData;//可设为全局变量
     if (Ref == true) {//清空表
         $(".nearby-data").empty();
         return;
@@ -35,6 +27,10 @@ function showData(Lng, Lat, Ref = false) {
             $(".nearby-data").empty();
             jsonData = eval(data);      //将data字符串转换为json数组            
             $(".nearby-data").append("<tr><th>UID</th><th>Uname</th><th>Note</th><th>Lng</th><th>Lat</th><th>Alt</th><th>Time</th></tr>");
+            
+            //将数据插入地图时只需要遍历jsonData 替换下面这段代码
+
+            //
             $.each(jsonData,function(index,elem){
                 $(".nearby-data").append("<tr>");
                 $(".nearby-data").append("<td>" + jsonData[index].UID + "</td>");
@@ -45,7 +41,8 @@ function showData(Lng, Lat, Ref = false) {
                 $(".nearby-data").append("<td>" + jsonData[index].Alt + "</td>");
                 $(".nearby-data").append("<td>" + jsonData[index].Time + "</td>");
                 $(".nearby-data").append("</tr>");
-            })        
+            })
+            //        
         },
         error: function (XMLHttpRequest, textStatus, errorThrown){
             $(".nearby-data").append("<tr><td>status:" + XMLHttpRequest.status + "</td><td>" + textStatus + "</td></tr>");    
@@ -57,6 +54,7 @@ function showData(Lng, Lat, Ref = false) {
     // xmlhttp.send();
 }
 
+//暂未测试
 function uploadData(UID, Uname, Note, Lng, Lat, Alt) {
     var xmlhttp;
     var data = "?UID=" + UID + "&Uname=" + Uname + "&Note=" + Note + "&Lng=" + Lon + "&Lat=" + Lat + "&Alt=" + Alt;

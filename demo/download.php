42f8660dfdf9d71aeb5eed988fd5a548b199b045
kagaya <kagaya85@foxmail.com>
2018-02-01 13:44:32 +0800

    删除调试用echo

--- demo/download.php
+++ demo/download.php
@@ -6,10 +6,10 @@ define('Range', '1');
 $SN = new spaceNote();
 
 if($_SERVER['REQUEST_METHOD']=='GET'){
-    echo "get request from client<br>";
-    echo "Longitude=".$_GET['Longitude']."Latirude=".$_GET['Latitude']."<br>";
+    // echo "get request from client<br>";
+    // echo "Longitude=".$_GET['Longitude']."Latirude=".$_GET['Latitude']."<br>";
     $SN->loadData($_GET['Longitude']-Range,$_GET['Longitude']+Range,$_GET['Latitude']-Range,$_GET['Latitude']+Range);
-    echo "loadData OK<br>";
+    // echo "loadData OK<br>";
 }
 
 ?>
